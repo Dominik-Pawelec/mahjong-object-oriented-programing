464f9c4c74939f42a2202e95b79cce97a80c51cc
Dominik-Pawelec <dominikpawelec@gmail.com>
2024-05-24 13:02:22 +0200

    started making class OpenBlock

--- Main.java
+++ Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args)
     {
 
-        Game game = new Game(new Human());
+        /*Game game = new Game(new Human());*/
 
+        Game game = new Game();
         game.startHanchan();
         game.prepareRound(new Hand(new Tile("1m"),new Tile("1m"),new Tile("1m"),new Tile("2m"),
         new Tile("3m"),new Tile("4m"),new Tile("5m"),new Tile("6m"),new Tile("7m"),new Tile("8m"),
---
+++ OpenBlock.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class OpenBlock extends TileGroup{
+    Tile stolen_tile;
+
+    
+
+    public OpenBlock(){
+        super();
+    }
+
+    public OpenBlock(Tile...args){
+        super(args);
+    }
+    public OpenBlock(OpenBlock copy){
+        super(copy);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
